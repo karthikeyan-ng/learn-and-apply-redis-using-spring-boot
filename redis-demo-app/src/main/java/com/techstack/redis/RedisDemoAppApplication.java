ea2df494556a7cb43782065e9557b2e9659edee6
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-04-05 16:25:28 +0530

    Created a project from start.spring.io and the same committed here.

---
+++ redis-demo-app/src/main/java/com/techstack/redis/RedisDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisDemoAppApplication.class, args);
+	}
+
+}
---
+++ redis-demo-app/src/test/java/com/techstack/redis/RedisDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
